0b640e2c0df3a1b89b83cf0de31a0e65acbad475
Κωνσταντίνος Κατσάβαρος <kkatsavaros@gmail.com>
2022-04-03 20:31:08 +0300

    commit initial

---
+++ ReadMe.txt
@@ -0,0 +1,22 @@
+ASP.NET Web Forms - JavaScript Confirm on Gridview Delete
+
+
+
+
+In that project i use Javascript to make o confirm dialog when i want to delete a row from a Grid View Control.
+
+
+
+ 
+1. I add an Grid View Control.
+
+
+2. I add a SqlDataSource Control.
+
+
+3. I create a RowDataBound event and i use from DataBinder class the Eval property to get the id of the row.
+
+
+
+
+Katsavaros Konstantinos   
---
+++ WebForm1.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace s02.JavaScriptConfirmOnGridviewDelete
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void GridView1_RowDataBound(object sender, GridViewRowEventArgs e)
+        {
+
+            if (e.Row.RowType == DataControlRowType.DataRow)
+            {
+                
+                LinkButton lb = (LinkButton)e.Row.FindControl("LinkButton1");
+
+                lb.Attributes.Add("onclick", "return confirm('Are you sure to delete record with ID = " + DataBinder.Eval(e.Row.DataItem, "ID") + "');");
+
+            }
+
+
+        }
+    }
+}
